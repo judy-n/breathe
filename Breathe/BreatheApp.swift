98378d39925cd297cc4bf8bf3ecb105a80c55cce
judy-n <judynaamani@icloud.com>
2022-02-28 18:50:17 -0500

    Initial Commit

---
+++ Breathe/BreatheApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreatheApp.swift
+//  Breathe
+//
+//  Created by Judy Naamani on 2/28/22.
+//
+
+import SwiftUI
+
+@main
+struct BreatheApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
